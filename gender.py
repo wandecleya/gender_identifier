0c450bd978dda75b1163b906260b0411ab778feb
Wandy <wandecleya@gmail.com>
2017-05-02 18:13:09 -0300

    Removes footnote of the pdf document

--- gender.py
+++ gender.py
@@ -1,12 +1,16 @@
-file = open("input.txt", "r")
+file = open('input.txt', 'r')
 unique_names = dict()
 
 for line in file:
     first_name = line.split(' ', 1 )[0]
-    if first_name in unique_names:
-        unique_names[first_name] += 1
+    if first_name == 'CONSULTE':
+        for i in range(3):
+            next(file)
     else:
-        unique_names[first_name] = 1
+        if first_name in unique_names:
+            unique_names[first_name] += 1
+        else:
+            unique_names[first_name] = 1
 
 for k, v in unique_names.items():
 	print (k + " " + str(v))
