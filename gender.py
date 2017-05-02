6922cb98154fc19f6174dbb5fc63ef07ef6cc24a
Wandy <wandecleya@gmail.com>
2017-05-02 18:35:09 -0300

    Removes email mistaken by name

--- gender.py
+++ gender.py
@@ -3,14 +3,18 @@ unique_names = dict()
 
 for line in file:
     first_name = line.split(' ', 1 )[0]
+
+    # Removes Pages 4 lines of Footnote
     if first_name == 'CONSULTE':
-        for i in range(3):
+        for i in range(4):
             next(file)
+            
+    if first_name in unique_names:
+        unique_names[first_name] += 1
+    elif '@' in first_name:
+        next(file)
     else:
-        if first_name in unique_names:
-            unique_names[first_name] += 1
-        else:
-            unique_names[first_name] = 1
+        unique_names[first_name] = 1
 
 counter = 0
 for k, v in unique_names.items():
