d53c030451c7ac16e2babd673b58430a158f1663
Wandy <wandecleya@gmail.com>
2017-05-09 15:48:55 -0300

    Filters names with numbers and less than 3 letters

--- gender.py
+++ gender.py
@@ -9,10 +9,18 @@ for line in full_list:
         for i in range(4):
             next(full_list)
 
+    #Removes emails mistaken by names
+    if '@' in first_name:
+        next(full_list)
+    #Remove names with numbers
+    elif any(not(letter.isalpha()) for letter in first_name):
+        next(full_list)
+    #Remove names with less than 3 letters
+    elif len(first_name) < 3:
+        next(full_list)
+
     if first_name in unique_names:
         unique_names[first_name] += 1
-    elif '@' in first_name:
-        next(full_list)
     else:
         unique_names[first_name] = 1
 
@@ -27,6 +35,8 @@ for k, v in unique_names.items():
     counter += 1
 
 total = ("\n Total: " + str(counter))
+print total
 result_file.write(total)
+
 
 result_file.close
